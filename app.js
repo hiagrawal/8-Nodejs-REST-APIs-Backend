d56cf2755f31ad0e8d13b11b0a6cca45395a865a
hiagrawal <agrawal.hina13@gmail.com>
2021-10-28 14:01:52 +0530

    Setting up a Post Model and Storing Posts in the database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -20,4 +21,11 @@ app.use((req,res,next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect('mongodb+srv://MongoDbUser:MongoDbUser@cluster0.kij6e.mongodb.net/RESTAPI?retryWrites=true&w=majority')
+.then(result => {
+    app.listen(8080);
+})
+.catch(err => {
+    console.log(err);
+})
+
