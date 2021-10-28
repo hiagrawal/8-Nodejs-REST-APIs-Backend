8f68a64ec9bc6371b2f4e2877efed0c7ab9bdae8
hiagrawal <agrawal.hina13@gmail.com>
2021-10-28 16:46:24 +0530

    Fetching a single post

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,18 +3,20 @@ const {validationResult} = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [{
-            title: 'First Post', 
-            content: 'This is the first post.', 
-            imageUrl: 'images/womens.png', 
-            creator: {
-                name: 'Hina Agrawal'
-            },
-            createdAt: new Date()
-
-        }]
-    });
+    Post.find().then(posts => {
+        if(!posts){
+            const error = new Error('No Post Found'); //this msg will automatically be set in error 'message' field
+            error.statusCode = 404; //storing status in any paramter lets say statusCode
+            throw error; //we are throwing here irrespective of inside async call coz this will throw and go to catch where we are doing next to this will work as we want
+        }
+        res.status(200).json({message: 'Fetched Posts successfully!', posts: posts});
+    })
+    .catch(err => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err); //throwing an error from here to go the common error handler in app js
+    })
 };
 
 //status 200 is just success, status 201 indicates that we created/added something which is more significant
@@ -56,3 +58,21 @@ exports.createPost = (req, res, next) => {
     })
     
 };
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId).then(post => {
+        if(!post){
+            const error = new Error('Post not found.'); //this msg will automatically be set in error 'message' field
+            error.statusCode = 404; //storing status in any paramter lets say statusCode
+            throw error; //we are throwing here irrespective of inside async call coz this will throw and go to catch where we are doing next to this will work as we want
+        }
+        res.status(200).json({message: 'Post Fetched', post: post});
+    })
+    .catch(err => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err); //throwing an error from here to go the common error handler in app js
+    })
+}
