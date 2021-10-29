21d41781f9d1734e649140c4b552d05c2cb34275
hiagrawal <agrawal.hina13@gmail.com>
2021-10-29 18:35:11 +0530

    Starting with User Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,7 +15,7 @@ exports.signUp = (req,res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
-    
+
     //with a salt of 12 so a strength of 12
     bcrypt.hash(password, 12).then(hashedPassword => {
         const user = new User({
@@ -27,6 +27,35 @@ exports.signUp = (req,res, next) => {
     })
     .then(result => {
         res.status(201).json({message: 'User Created!', userId: result._id});
+    })
+    .catch(err => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    })
+};
+
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+    User.findOne({email: email}).then(userDoc => {
+        if(!userDoc){
+            const error = new Error('Email Not found');
+            error.statusCode = 401; //401 refers to authentication error
+            throw error;
+        }
+        loadedUser = userDoc;
+        return bcrypt.compare(password, userDoc.password)
+    })
+    .then(isEqual => {
+        if(!isEqual){
+            const error = new Error('Password is not correct');
+            error.statusCode = 401; //401 refers to authentication error
+            throw error;
+        }
+    
     })
     .catch(err => {
         if(!err.statusCode){
